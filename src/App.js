81575042d9bd72b808d4cf14cc5679068538e53a
saadman  ahmed <saadman.ahm@gmail.com>
2019-08-10 23:21:08 +0800

    sss

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Titles from './Titles'
 import Weather from './Weather'
 
 
+console.log("hello bboy")
+const Api_Key='a8bd6f56e0c7055c333393779d370ddc'
+
 class App extends React.Component{
     state = {
         temperature: undefined,
@@ -15,16 +18,43 @@ class App extends React.Component{
         description: undefined,
         error: undefined
       }
+      
+      getWeather = async (e) => {
+        e.preventDefault();  
+
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        console.log("hello bboy")
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+        const response = await api_call.json();
+        console.log("hello bboy")
+        console.log(response);
+        console.log("hello bboy")
+        if(city && country){
+          this.setState({
+            temperature: response.main.temp,
+            city: response.name,
+            country: response.sys.country,
+            humidity: response.main.humidity,
+            description: response.weather[0].description,
+            error: ""
+          })
+        }else{
+          this.setState({
+            error: "Please input search values..."
+          })
+        }
+      }
 
 render(){
-    this.getWeather=this.getWeather.bind(this)
+    
 
 return(
 <div>
 
 <h1>Hello there!</h1>
 <Titles/>
-<Form loadtWeather={this.getWeather} />
+<Form getWeather={this.getWeather} />
 <Weather  temperature={this.state.temperature}
           city={this.state.city}
           country={this.state.country}
@@ -42,25 +72,7 @@ return(
 
 }
 
-getWeather = async (e) => {
-const Api_Key='a8bd6f56e0c7055c333393779d370ddc'
-const city = e.target.elements.city.value;
 
-const country = e.target.elements.country.value;
-const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
-  
-const response = await api_call.json();
-
-    e.preventDefault();
-    this.setState({
-        temperature: response.main.temp,
-        city: response.name,
-        country: response.sys.country,
-        humidity: response.main.humidity,
-        description: response.weather[0].description,
-        error: ""
-      })
-}
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,13 @@
 import React from 'react';
 const Form = (props) => {
   return (
-      <form onSubmit={this.props.loadWeather}>
+      <form onSubmit={this.props.getWeather}>
         <input type="text" name="city" placeholder="City..." />
         <input type="text" name="country" placeholder="Country..." />
         <button>Get Weather</button>
       </form>
   )
 }
-getWeather = async () => {
 
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${Api_Key}`);
-  
-  const response = await api_call.json();
-  
-  console.log(response);
-  
-}
 
 export default Form;
